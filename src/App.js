0ee8d076c36bbf7792f6459224125ee7dd83447e
cromx123 <73903821+cromx123@users.noreply.github.com>
2024-11-14 01:11:23 -0300

    actualización pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home";
+import Model from "./components/Model";
+import About from "./components/About";
+
 
 function App() {
+  const [activeLink, setActiveLink] = useState("home");
+
+  const handleNavClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav className="navbar">
+            <div className="navbar-izq">
+              <div className="circle-logo"></div>
+              <ul className="navbar-links">
+                <li><Link to="/" className={activeLink === "home" ? "active" : ""} onClick={() => handleNavClick("home")}>HOME</Link></li>
+                <li><Link to="/models" className={activeLink === "models" ? "active" : ""} onClick={() => handleNavClick("models")}>MODELS</Link></li>
+                <li><Link to="/about" className={activeLink === "about" ? "active" : ""} onClick={() => handleNavClick("about")}>ABOUT</Link></li>
+              </ul>
+            </div>
+            <div className="navbar-der">
+              <button>SIGN IN</button>
+              <button>SIGN UP</button>
+            </div>
+          </nav>
+        </header>
+        <body>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/models" element={<Model />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </body>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Model.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Model_1() {
+  return (
+    <div>
+      <h1>Model Us</h1>
+      <p>This is the About page.</p>
+    </div>
+  );
+}
+
+export default Model_1;
